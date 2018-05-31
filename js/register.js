96b0e0ccfb35fd733bb7ac7bf96d322fd93a1f46
marcf08 <marcus.flores09@gmail.com>
2018-05-30 20:01:43 -0400

    Working via Cognito

--- js/register.js
+++ js/register.js
@@ -1,34 +1,57 @@
 let noMatch = "Passwords do not match.";
 let badInput = "Input error.";
 let success = "Success!";
-let uri = "https://qktvnkadoj.execute-api.us-east-1.amazonaws.com/staging/dbadduser";
 
-let register = function(username, password, confirmPassword) {
+var data = {
+  UserPoolId: '', // Insert your user pool id
+  ClientId: '' // Insert your app client id
+};
+var userPool = new AmazonCognitoIdentity.CognitoUserPool(data);
+
+let register = function(username, registerUserRealName, registerUserLocale, password, confirmPassword) {
   if (username == "" || password == "" || confirmPassword == "") {
-        $('#registerModal').modal('hide');
-        $('#errorModal').modal('show');
-        $('#msgError').text(badInput);
-        return;
+    $('#registerModal').modal('hide');
+    $('#errorModal').modal('show');
+    $('#msgError').text(badInput);
+    return;
   }
   if (password != confirmPassword) {
-        $('#registerModal').modal('hide');
-        $('#errorModal').modal('show');
-        $('#msgError').text(noMatch);
-        return;
+    console.log(password);
+    console.log(confirmPassword);
+    $('#registerModal').modal('hide');
+    $('#errorModal').modal('show');
+    $('#msgError').text(noMatch);
+
+    return;
   }
   if (password === confirmPassword) {
     $('#registerModal').modal('hide');
     $('#confirmModal').modal('show');
-    $.ajax({
-      type: "POST",
-      url: uri,
-      headers: {
-        "x-api-key": api_key
-      },
-      data: {
-        "username": username,
-        "password": confirmPassword
-      }
+
+    var dataName = {
+      Name : 'name',
+      Value : registerUserRealName
+    };
+    var dataLocale = {
+      Name: 'locale',
+      Value: registerUserLocale
+    };
+
+    var attributeList = [];
+
+    var attributeName = new AmazonCognitoIdentity.CognitoUserAttribute(dataName);
+    var attributeLocale = new AmazonCognitoIdentity.CognitoUserAttribute(dataLocale);
+
+    attributeList.push(attributeName);
+    attributeList.push(attributeLocale);
+
+    userPool.signUp(username, confirmPassword, attributeList, null, function(err, result){
+        if (err) {
+            console.log(err);
+            return;
+        }
+        cognitoUser = result.user;
+        console.log('user name is ' + cognitoUser.getUsername());
     });
   }
 }
