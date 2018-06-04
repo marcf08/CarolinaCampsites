d3cf03b9cb5e537a3542f1dea3e76a15e17a7368
marcf08 <marcus.flores09@gmail.com>
2018-06-03 21:21:55 -0400

    Add marker styling

--- js/camp.js
+++ js/camp.js
@@ -9,23 +9,24 @@ map = new mapboxgl.Map({
   style: 'mapbox://styles/marcf08/cjhxpdtwh2n5i2rpbhckpno9h'
 });
 
-
 map.on('mouseup', function(e) {
   if (e.originalEvent.button == 2) {
     if (map.getZoom() < 15.5) {
       $('#ZoomInModal').modal('show');
       return;
     }
-  let tempLat = e.lngLat.lat;
-  let tempLong = e.lngLat.lng;
-  $('#addToMapModal').modal('show');
-  $('#resourceLatitude').val(tempLat);
-  $('#resourceLongitude').val(tempLong);
+    let tempLat = e.lngLat.lat;
+    let tempLong = e.lngLat.lng;
+    $('#addToMapModal').modal('show');
+    $('#resourceLatitude').val(tempLat);
+    $('#resourceLongitude').val(tempLong);
   }
 });
 
 let addMarker = function(resourceType, lat, lng) {
-  let marker = new mapboxgl.Marker()
-  .setLngLat([lng, lat])
-  .addTo(map);
+  let el = document.createElement('div');
+  el.className = 'marker';
+  let marker = new mapboxgl.Marker(el)
+    .setLngLat([lng, lat])
+    .addTo(map);
 }
--- js/signon.js
+++ js/signon.js
@@ -1,82 +1,61 @@
 /**
 Used to sign in a user and establish a session with Cognito.
 **/
+var cognitoUser;
+var poolData = {
+  UserPoolId: 'us-east-1_evimZrQDn',
+  ClientId: '689in92k06pd12lu9um7tl4bmb'
+}
 
-let user;
+window.onload = function() {
+  let userPool = new AWSCognito.CognitoIdentityServiceProvider.CognitoUserPool(poolData);
+  cognitoUser = userPool.getCurrentUser();
+  if (cognitoUser != undefined || cognitoUser != null) {
+    $('#loginLogout').text('Logout');
+    $('#loginLogout').attr("data-toggle", "");
+    $('#loginLogout').attr("data-target", "");
+    $('#loginLogout').attr("onclick", "logout()");
+  }
+}
 
-var credKeys = [
-  'accessKeyId',
-  'secretAccessKey',
-  'sessionToken'
-];
-
-let signon = function(username, password) {
+var signon = function(username, password) {
   let authenticationData = {
     Username: username,
     Password: password,
   };
-
-  //May change this to aws cognito identity service provider
   let authenticationDetails = new AmazonCognitoIdentity.AuthenticationDetails(authenticationData);
-
-  let poolData = {
-    UserPoolId: 'us-east-1_evimZrQDn',
-    ClientId: '689in92k06pd12lu9um7tl4bmb'
-  };
-
   let userPool = new AmazonCognitoIdentity.CognitoUserPool(poolData);
-
   let userData = {
     Username: username,
     Pool: userPool
   };
-
-  let cognitoUser = new AmazonCognitoIdentity.CognitoUser(userData);
-
+  cognitoUser = new AmazonCognitoIdentity.CognitoUser(userData);
   cognitoUser.authenticateUser(authenticationDetails, {
     onSuccess: function(result) {
       user = cognitoUser;
-      console.log('access token + ' + result.getAccessToken().getJwtToken());
-      /*Use the idToken for Logins Map when Federating User Pools with identity pools or when passing through an Authorization Header to an API Gateway Authorizer*/
-      console.log('idToken + ' + result.idToken.jwtToken);
+      //console.log('access token + ' + result.getAccessToken().getJwtToken());
+      //console.log('idToken + ' + result.idToken.jwtToken);
       cognitoUser.getSession(function(err, session) {
-            if (err) {
-                alert(err.message || JSON.stringify(err));
-                return;
-            }
-            console.log('session validity: ' + session.isValid());
-
-            // NOTE: getSession must be called to authenticate user before calling getUserAttributes
-            cognitoUser.getUserAttributes(function(err, attributes) {
-                if (err) {
-                    // Handle error
-                } else {
-                    // Do something with attributes
-                }
-            });
-
-            AWS.config.credentials = new AWS.CognitoIdentityCredentials({
-                IdentityPoolId : '...', // your identity pool id here
-                Logins : {
-                    // Change the key below according to the specific region your user pool is in.
-                    'cognito-idp.<region>.amazonaws.com/<YOUR_USER_POOL_ID>' : session.getIdToken().getJwtToken()
-                }
-            });
-
-            // Instantiate aws sdk service objects now that the credentials have been updated.
-            // example: var s3 = new AWS.S3();
-
-        });
-      console.log(result);
-      cognitoUser.getUserAttributes(function(err, result) {
         if (err) {
-          alert(err);
+          alert(err.message || JSON.stringify(err));
           return;
         }
-        for (i = 0; i < result.length; i++) {
-          console.log('attribute ' + result[i].getName() + ' has value ' + result[i].getValue());
-        }
+        //console.log('session validity: ' + session.isValid());
+        cognitoUser.getUserAttributes(function(err, attributes) {
+          if (err) {
+            // Handle error
+          } else {
+            console.log(attributes);
+          }
+        });
+        AWS.config.credentials = new AWS.CognitoIdentityCredentials({
+          IdentityPoolId: '689in92k06pd12lu9um7tl4bmb',
+          Logins: {
+            'cognito-idp.us-east-1.amazonaws.com/us-east-1_evimZrQDn': session.getIdToken().getJwtToken()
+          }
+        });
       });
+      //console.log(result);
       $('#loginModal').modal('hide');
       $('#loginLogout').text('Logout');
       $('#loginLogout').attr("data-toggle", "");
@@ -92,7 +71,25 @@ let signon = function(username, password) {
   });
 }
 
-let logout = function() {
-  //console.log(AWS.config.credentials.get());
+var logout = function() {
+  cognitoUser.signOut();
   location.reload();
+}
+
+var isLoggedIn = function() {
+  let userPool = new AWSCognito.CognitoIdentityServiceProvider.CognitoUserPool(poolData);
+  cognitoUser = userPool.getCurrentUser();
+  if (cognitoUser != undefined) {
+    return true;
+  }
+  if (cognitoUser != null) {
+    cognitoUser.getSession(function(err, session) {
+      if (err) {
+        alert(err);
+        return false;
+      }
+      return true;
+      //console.log('session validity: ' + session.isValid());
+    });
+  }
 }
